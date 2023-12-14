ca9c002b1e937103c2ef053d7cd9140fa95ce1b0
KoichiMin <koichidminami@gmail.com>
2023-12-14 11:42:18 -0500

    added checkout option

--- src/main/java/com/pluralsight/Screen.java
+++ src/main/java/com/pluralsight/Screen.java
@@ -10,32 +10,33 @@ public class Screen {
     public static void main(String[] args) {
 
 
-        Scanner  scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Book[] books = new Book[20];
-        books[0] = new Book(1, "ISBN001", "Book 1", false, "");
-        books[1] = new Book(2, "ISBN002", "Book 2", false, "");
-        books[2] = new Book(3, "ISBN003", "Book 3", false, "");
-        books[3] = new Book(4, "ISBN004", "Book 4", false, "");
-        books[4] = new Book(5, "ISBN005", "Book 5", true, "john");
-        books[5] = new Book(6, "ISBN006", "Book 6", false, "");
-        books[6] = new Book(7, "ISBN007", "Book 7", false, "");
-        books[7] = new Book(8, "ISBN008", "Book 8", false, "");
-        books[8] = new Book(9, "ISBN009", "Book 9", false, "");
-        books[9] = new Book(10, "ISBN010", "Book 10", false, "");
-        books[10] = new Book(11, "ISBN011", "Book 11", false, "");
-        books[11] = new Book(12, "ISBN012", "Book 12", false, "");
-        books[12] = new Book(13, "ISBN013", "Book 13", false, "");
-        books[13] = new Book(14, "ISBN014", "Book 14", true, "jimmy");
-        books[14] = new Book(15, "ISBN015", "Book 15", false, "");
-        books[15] = new Book(16, "ISBN016", "Book 16", false, "");
-        books[16] = new Book(17, "ISBN017", "Book 17", false, "");
-        books[17] = new Book(18, "ISBN018", "Book 18", false, "");
-        books[18] = new Book(19, "ISBN019", "Book 19", true, "bob");
-        books[19] = new Book(20, "ISBN020", "Book 20", false, "");
+        books[0] = new Book(1, "ISBN001", "To Kill a Mockingbird", false, "");
+        books[1] = new Book(2, "ISBN002", "1984", false, "");
+        books[2] = new Book(3, "ISBN003", "The Catcher in the Rye", false, "");
+        books[3] = new Book(4, "ISBN004", "The Great Gatsby", false, "");
+        books[4] = new Book(5, "ISBN005", "Animal Farm", true, "John");
+        books[5] = new Book(6, "ISBN006", "Lord of the Flies", false, "");
+        books[6] = new Book(7, "ISBN007", "The Grapes of Wrath", false, "");
+        books[7] = new Book(8, "ISBN008", "Brave New World", false, "");
+        books[8] = new Book(9, "ISBN009", "One Hundred Years of Solitude", false, "");
+        books[9] = new Book(10, "ISBN010", "Fahrenheit 451", false, "");
+        books[10] = new Book(11, "ISBN011", "The Lord of the Rings", false, "");
+        books[11] = new Book(12, "ISBN012", "The Hobbit", false, "");
+        books[12] = new Book(13, "ISBN013", "The Kite Runner", false, "");
+        books[13] = new Book(14, "ISBN014", "Pride and Prejudice", true, "Jimmy");
+        books[14] = new Book(15, "ISBN015", "The Outsiders", false, "");
+        books[15] = new Book(16, "ISBN016", "Slaughterhouse-Five", false, "");
+        books[16] = new Book(17, "ISBN017", "Catch-22", false, "");
+        books[17] = new Book(18, "ISBN018", "The Fountainhead", false, "");
+        books[18] = new Book(19, "ISBN019", "The Hitchhiker's Guide to the Galaxy", true, "Bob");
+        books[19] = new Book(20, "ISBN020", "The Shining", false, "");
+
 
         boolean displayOptions = true;
 
-        while(displayOptions) {
+        while (displayOptions) {
 
             System.out.println("A - Show Available Books");
             System.out.println("S - Show Checked Out Book");
@@ -53,6 +54,7 @@ public class Screen {
                     break;
                 case 's':
                     System.out.println("S has been pressed - Show Checked Out Book");
+                    displayCheckedOutBooks(books);
                     break;
                 case 'c':
                     System.out.println("C has been pressed - Check In a Book");
@@ -71,20 +73,64 @@ public class Screen {
         }
 
 
-
     }
 
 
-    public static void  displayAvailableBooks(Book[] books) {
-        for(Book book : books){
-            if(book.isCheckedOut == false){
+    public static void displayAvailableBooks(Book[] books) {
+        Scanner scanner = new Scanner(System.in);
+        for (Book book : books) {
+            if (book.isCheckedOut == false) {
                 System.out.println(
-                "Book{" +
-                        "id=" + book.id +
-                        ", isbn='" + book.isbn + '\'' +
-                        ", title='" + book.title + '\'' +
-                        '}');
+                        "title = " + book.title + '\'' +
+                                ", id = " + book.id +
+                                ", isbn = '" + book.isbn + '\''
+                );
             }
         }
+        System.out.print("Would you like to checkout a book? Y -- Yes or N -- No:  ");
+        char choice = scanner.next().charAt(0);
+        scanner.nextLine();
+        if (Character.toLowerCase(choice) == 'y') {
+            System.out.print("Please select a book : ");
+            String choiceBook = scanner.nextLine();
+            for (Book book : books) {
+                if(book.title.equals(choiceBook)){
+                    checkOutBook(book);
+                }
+            }
+
+        } else {
+//            bring user back to the home screen options
+        }
+    }
+
+
+    public static void displayCheckedOutBooks(Book[] books) {
+        for (Book book : books) {
+            if (book.isCheckedOut) {
+                System.out.println(
+                        ", title='" + book.title + '\'' +
+                                "id=" + book.id +
+                                ", isbn='" + book.isbn + '\''
+                );
+            }
+        }
+    }
+
+
+    public static void checkOutBook(Book book) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the name for book checkout : ");
+        String name = scanner.nextLine();
+        book.checkOut(name);
+        System.out.print("Thank you for checking out a book, would you like to go back to the home screen? Y -- Yes or N -- No:");
+        char choice = scanner.next().charAt(0);
+        if(Character.toLowerCase(choice) == 'y'){
+//            go back to home screen
+        } else{
+            System.out.println("Thank you for coming to Koichi's Library!");
+            System.exit(0);
+        }
+
     }
 }
